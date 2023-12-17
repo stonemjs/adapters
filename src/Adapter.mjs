2f749502c4bfd1212b337d6885186fe4e67c25e2
Mr Stone <pierre.evens16@gmail.com>
2023-12-16 23:45:12 -0500

    Implement default Adapter

---
+++ src/Adapter.mjs
@@ -0,0 +1,32 @@
+import { isFunction } from '@stone-js/core'
+import { StoneFactory } from '@stone-js/core'
+import { LogicException } from '@stone-js/core'
+
+export class Adapter {
+  #context
+  #appModule
+  #configurations
+
+  constructor (app, configurations = {}) {
+    if (app instanceof StoneFactory) {
+      this.#context = app
+    } else if (isFunction(app)) {
+      this.#appModule = app
+      this.#configurations = configurations
+    } else {
+      throw new LogicException('The first argument must be an instance of StoneFactory or your AppModule(function or class).')
+    }
+  }
+
+  static create (app, configurations = {}) {
+    return new this(app, configurations)
+  }
+
+  getContext () {
+    return this.#context ?? StoneFactory.create(this.#appModule, this.#configurations)
+  }
+
+  run () {
+    return this.getContext().run()
+  }
+}
---
+++ tests/Adapter.test.js
@@ -0,0 +1,5 @@
+describe('Adapter', () => {
+  test('Init', () => {
+    expect(true).toEqual(true)
+  })
+})
