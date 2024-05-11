0ad1d56dc9ec6c8d21d5868d0b8173fb71e6e95d
Mr Stone <pierre.evens16@gmail.com>
2024-05-11 18:31:18 -0400

    Normalize

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
 import multi from '@rollup/plugin-multi-entry'
 import commonjs from '@rollup/plugin-commonjs'
@@ -7,29 +6,21 @@ import nodeExternals from 'rollup-plugin-node-externals'
 
 const inputs = {
   decorators: 'src/decorators/*.mjs',
-  index: ['src/Mapper.mjs', 'src/constants.mjs', 'src/adapters/*.mjs'],
+  index: [
+    'src/Mapper.mjs',
+    'src/constants.mjs',
+    'src/adapters/*.mjs'
+  ],
 }
 
 export default Object.entries(inputs).map(([name, input]) => ({
 	input,
 	output: [
-    { format: 'es', file: `dist/${name}.mjs` },
-    { format: 'cjs', file: `dist/${name}.cjs` }
+    { format: 'es', file: `dist/${name}.js` }
   ],
-  onwarn: (warning, handler) => {
-    if (
-      ['CIRCULAR_DEPENDENCY', 'THIS_IS_UNDEFINED'].includes(warning.code) &&
-      [].concat(warning.id, warning.ids).filter(v => v?.includes('/node_modules/')).length // Disable for node_modules
-    ) {
-      return
-    } else {
-      handler(warning)
-    }
-  },
   plugins: [
-    json(),
     multi(),
-    nodeExternals({ deps: false }), // Must always be before `nodeResolve()`.
+    nodeExternals(), // Must always be before `nodeResolve()`.
     nodeResolve({
       exportConditions: ['node', 'import', 'require', 'default']
     }),
--- src/constants.mjs
+++ src/constants.mjs
@@ -1,4 +1,3 @@
 export const BROWSER_PLATFORM = 'browser'
 export const NODE_HTTP_PLATFORM = 'node_http'
 export const AWS_LAMBDA_PLATFORM = 'aws_lambda'
-export const NODE_CONSOLE_PLATFORM = 'node_console'
